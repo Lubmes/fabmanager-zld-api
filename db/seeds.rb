2144d4cc3c27a5618504aabb8f07c3d652efa4fd
Lubmes <l.m.schukking@gmail.com>
2017-09-09 10:32:58 +0200

    Bewerking seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# Fabmoment.delete_all
+# User.delete_all
 
 def create_fabmoment(user, title)
   fabmoment = user.fabmoments.find_or_create_by(title: title)
@@ -19,13 +23,20 @@ unless User.exists?(email: "info@fablabzeeland.com")
                         password: "12345",
                         admin: true)
 end
-user_2 = User.create!(username: Faker::Internet.user_name,
-                      email: Faker::Internet.free_email,
-                      password: "12345")
-user_3 = User.create!(username: Faker::Internet.user_name,
-                      email: Faker::Internet.free_email,
-                      password: "12345")
 
+def create_user(admin = false)
+  name = Faker::Internet.user_name
+  user = User.create!(username: name,
+                      email: Faker::Internet.free_email(name),
+                      password: "12345",
+                      admin: admin)
+end
+
+user_2 = create_user
+user_3 = create_user
+
+create_fabmoment(user_1, Faker::App.name)
+create_fabmoment(user_1, Faker::App.name)
 create_fabmoment(user_2, Faker::App.name)
 create_fabmoment(user_2, Faker::App.name)
 create_fabmoment(user_2, Faker::App.name)
